9f47eb0f33e06044f34436694de09e4b96a5db5b
garu <12garu12@gmail.com>
2025-03-19 21:32:15 -0500

    inicio del proyecto customers para mi app

---
+++ customers/src/test/java/co/com/mybank/customers/CustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mybank.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
